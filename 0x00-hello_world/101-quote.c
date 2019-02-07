5d5b307e46c03bf8796f22f54317c4aaec81cb97
Arthur Damm <indigoarthur@gmail.com>
2019-02-07 01:08:08 +0000

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,8 +8,8 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, "
-		"2015-10-19\n");
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
 

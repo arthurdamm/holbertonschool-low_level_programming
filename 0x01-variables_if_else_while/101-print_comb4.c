3e613ee86b15451c65942e8cab2aec2d8bc1f058
Arthur Damm <indigoarthur@gmail.com>
2019-02-08 01:00:49 +0000

    add 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != '7' || b != '8' || c != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}

09555df9e43812067130976cf14e871304a799d1
Arthur Damm <indigoarthur@gmail.com>
2019-02-08 00:22:40 +0000

    add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+		if (c < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}

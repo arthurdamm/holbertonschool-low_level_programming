2306de0e06b1eb72691bf56143a7d68506cc9990
Arthur Damm <indigoarthur@gmail.com>
2019-02-11 23:55:37 +0000

    add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints characters from a to z
+ *
+ * Return: Always void.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}

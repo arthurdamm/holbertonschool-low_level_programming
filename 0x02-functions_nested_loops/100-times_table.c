df3efdbe63983acb0c3c96cdaf8d826f7610308d
Arthur Damm <indigoarthur@gmail.com>
2019-02-12 22:58:57 +0000

    add 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,41 @@
 #include "holberton.h"
 
 /**
- * print_times_table - prints the times-table from 0 to n
- * @n: an integer from 0 to 15 inclusive
- *
- * Returns: void
- */
+* print_times_table - print a times table with only putchar
+* @n: passed in number to act as table size
+*
+* Return: void
+*/
+
 void print_times_table(int n)
 {
-	int i, j, k, h, t, o;
+	int x;
+	int y;
+	int prod;
 
-	if (n < 0 || n > 15)
-		return;
-
-	for (i = 0; i <= n; i++)
+	if (n <= 15 && n >= 0)
 	{
-		for (j = 0; j <= n; j++)
+		for (y = 0; y <= n; y++)
 		{
-			k = i * j;
-
-			h = k / 100;
-			t = (k % 100) / 10;
-			o = k % 10;
-			if (h)
-				_putchar(h + '0');
-			else if (j)
-				_putchar(' ');
-			if (t)
-				_putchar(t + '0');
-			else if (h)
-				_putchar('0');
-			else if (j)
-				_putchar(' ');
-			_putchar(o + '0');
-			if (j < n)
+			_putchar ('0');
+			for (x = 1; x <= n; x++)
 			{
+				prod = x * y;
 				_putchar(',');
 				_putchar(' ');
+				if (prod > 99)
+					_putchar(prod / 100 + '0');
+				else
+					_putchar(' ');
+				if (prod > 9)
+					_putchar((prod / 10)  % 10 + '0');
+				else
+					_putchar(' ');
+				_putchar(prod % 10 + '0');
+				if (x == n)
+				_putchar('\n');
 			}
 		}
-		_putchar('\n');
 	}
 }
+

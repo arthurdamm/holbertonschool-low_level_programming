34f524fd56e164acc01feb71685446b04e9cf84d
Arthur Damm <indigoarthur@gmail.com>
2019-02-12 02:14:35 +0000

    add 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _abs - returns the absolute value of integer
+ * @n: the integer to value absolutely
+ *
+ * Return: a positive integer or zero
+ */
+int _abs(int n)
+{
+	return (n > 0 ? n : -n);
+}

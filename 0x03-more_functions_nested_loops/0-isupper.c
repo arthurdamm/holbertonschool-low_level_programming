61d5364135878e4cde77895256edc8e853c74725
Arthur Damm <indigoarthur@gmail.com>
2019-02-14 08:55:52 +0000

    add 0-isupper.c

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks if character is upper case
+ * @c: the character to check case
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}

bbb8f9e74a164c70daee11c6e19421f42a8e2072
Arthur Damm <indigoarthur@gmail.com>
2019-02-14 21:51:03 +0000

    add holberton.h

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -5,4 +5,24 @@ int _putchar(char);
 
 int _isupper(int c);
 
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+int is_prime(long n);
+
 #endif

90986e7b8fc5132cd2b01a6728b1441ad9d24046
Arthur Damm <indigoarthur@gmail.com>
2019-02-19 23:01:10 +0000

    add 101-keygen.c

---
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define SUM 2772
+
+/**
+ * main - it all starts here
+ *
+ * Return: 0 always success
+ */
+int main(void)
+{
+	int sum = 0, n = 0, i = 0;
+	char str[SUM / 48 + 1];
+
+	srand(time(NULL));
+	while (sum < SUM)
+	{
+		if (SUM - sum < 48)
+			sum -= str[--i];
+		else if (SUM - sum <= 126)
+			n = SUM - sum;
+		else
+			n = rand() % (126 - 48) + 48;
+		if (n)
+		{
+			str[i++] = n;
+			sum += n;
+		}
+		n = 0;
+	}
+	str[i] = '\0';
+	printf("%s", str);
+	return (0);
+}

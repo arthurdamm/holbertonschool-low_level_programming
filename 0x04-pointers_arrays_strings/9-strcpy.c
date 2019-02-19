3f005049c4f6d71e4f866efd4fecc7d73b608e89
Arthur Damm <indigoarthur@gmail.com>
2019-02-19 20:26:26 +0000

    add 9-strcpy.c

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -13,11 +13,11 @@ char *_strcpy(char *dest, char *src)
 
 	if (dest == src || src == 0)
 		return (dest);
-
-	while (src[i] != '\0')
+	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = src[i];
 	return (dest);
 }

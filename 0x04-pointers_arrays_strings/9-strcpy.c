45c84bb1152f5c5f901b8b29f875cebaae685d7f
Arthur Damm <arthurdamm@Arthurs-MacBook-Air.local>
2019-02-19 10:17:47 -0800

    add 9-strcpy.c

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -10,16 +10,10 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
-
-	if (dest == src || src == 0)
-		return (dest);
-
-	while (src[i] != '\0')
+	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	if (src[i] == '\0')
-		dest[i] = src[i];
 	return (dest);
 }

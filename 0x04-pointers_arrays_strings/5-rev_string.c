a281b4af93963c3222fcf87e0f9b20a51d237d82
Arthur Damm <arthurdamm@Arthurs-MacBook-Air.local>
2019-02-19 01:50:11 -0800

    add 5-rev_string.c

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,14 @@
  * Return: void
  */
 void rev_string(char *s)
-{	
+{
 	int l = 0, i = 0;
 	char t;
 
 	for (i = 0; s[i]; i++)
 		l++;
 
-	for(i = 0; i < l / 2; i++)
+	for (i = 0; i < l / 2; i++)
 	{
 		t = s[l - i - 1];
 		s[l - i - 1] = s[i];

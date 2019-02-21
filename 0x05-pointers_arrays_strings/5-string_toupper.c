bfe2b86dcaac1716c7e80d2434865594fb721b0b
Arthur Damm <indigoarthur@gmail.com>
2019-02-21 21:13:34 +0000

    add 5-string_toupper.c

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - converts strings to uppercase
+ * @s: the string to convert
+ *
+ * Return: void
+ */
+char *string_toupper(char *s)
+{
+	for (; *s; s++)
+		if (*s >= 'a' && *s <= 'z')
+			*s += 'A' - 'a';
+	return (s);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -13,4 +13,6 @@ int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
 
+char *string_toupper(char *);
+
 #endif

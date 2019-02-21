5c1559687a0d48293a846ab6ac15ff2b38a1941f
Arthur Damm <indigoarthur@gmail.com>
2019-02-21 21:16:16 +0000

    add 5-string_toupper.c

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -4,11 +4,12 @@
  * string_toupper - converts strings to uppercase
  * @s: the string to convert
  *
- * Return: void
+ * Return: char pointer
  */
 char *string_toupper(char *s)
 {
 	char *ret = s;
+
 	for (; *s; s++)
 		if (*s >= 'a' && *s <= 'z')
 			*s += 'A' - 'a';

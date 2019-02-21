2a1f80febaa3444f911b87f9685a0b4e517ee455
Arthur Damm <indigoarthur@gmail.com>
2019-02-21 22:32:34 +0000

    add 7-leet.c

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -4,21 +4,6 @@
 #define LEETOUT "4433007711"
 
 /**
- * member - tests if a string contains a character
- * @c: the character
- * @s: the string
- *
- * Return: 1 if found, 0 otherwise
- */
-int leetreplace(char c, char *s)
-{
-	for (; *s; s++)
-		if (*s == c)
-			return (1);
-	return (0);
-}
-
-/**
  * leet - encodes a string into 1337
  * @s: the string to encode
  *

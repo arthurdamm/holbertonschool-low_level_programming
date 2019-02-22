5805807fc4ffa2e01de154c0d167afe607e19280
Arthur Damm <indigoarthur@gmail.com>
2019-02-22 05:03:24 +0000

    add 103-print_buffer.c

---
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints string buffer
+ * @b: the buffer
+ * @size: size of buffer to print
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int o = 0, i, j;
+
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+				printf(" ");
+		}
+		printf(" ");
+		for (i = 0; i < 10; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+				c = '.';
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -25,4 +25,6 @@ void print_number(int n);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
+void print_buffer(char *b, int size);
+
 #endif

76c5e9d76531e60565f66d5e68aee7a68de53007
Arthur Damm <indigoarthur@gmail.com>
2019-02-28 01:35:51 +0000

    add 100-wildcmp.c

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+ * wildcmp - compares two strings lexicographically
+ * @s1: the first string
+ * @s2: the second string, can contain wildcard
+ *
+ * Return: 1 if identical, 0 if false
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		while (*(s2 + 1) == '*')
+			s2++;
+		s2++;
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		while (*++s1)
+			if (*s1 == *s2)
+				ret += wildcmp(s1 + 1, s2 + 1);
+		return (!!ret);
+	}
+	return (0);
+}

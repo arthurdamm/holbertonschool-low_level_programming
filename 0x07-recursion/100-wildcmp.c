51d6ef07c266bb5a487d2c8b15ba000641de363b
Arthur Damm <indigoarthur@gmail.com>
2019-02-28 01:52:07 +0000

    add 100-wildcmp.c

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -1,6 +1,39 @@
 #include "holberton.h"
 
 /**
+ * move_past_star - iterates past asterisk
+ * @s2: the second string, can contain wildcard
+ *
+ * Return: the pointer past star
+ */
+char *move_past_star(char *s2)
+{
+	if (*s2 == '*')
+		return (move_past_star(s2 + 1));
+	else
+		return (s2);
+}
+
+/**
+ * inception - makes magic a reality
+ * @s1: the first string
+ * @s2: the second string, can contain wildcard
+ *
+ * Return: 1 if identical, 0 if false
+ */
+int inception(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (*s1 == 0)
+		return (0);
+	if (*s1 == *s2)
+		ret += wildcmp(s1 + 1, s2 + 1);
+	ret += inception(s1 + 1, s2);
+	return (ret);
+}
+
+/**
  * wildcmp - compares two strings lexicographically
  * @s1: the first string
  * @s2: the second string, can contain wildcard
@@ -11,6 +44,8 @@ int wildcmp(char *s1, char *s2)
 {
 	int ret = 0;
 
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+		return (1);
 	if (*s1 == *s2)
 	{
 		if (!*s1)
@@ -21,16 +56,12 @@ int wildcmp(char *s1, char *s2)
 		return (0);
 	if (*s2 == '*')
 	{
-		while (*(s2 + 1) == '*')
-			s2++;
-		s2++;
+		s2 = move_past_star(s2);
 		if (!*s2)
 			return (1);
 		if (*s1 == *s2)
 			ret += wildcmp(s1 + 1, s2 + 1);
-		while (*++s1)
-			if (*s1 == *s2)
-				ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
 		return (!!ret);
 	}
 	return (0);

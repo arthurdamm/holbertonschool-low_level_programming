878f969db1749e73333da74433b66092ef8f1868
Arthur Damm <indigoarthur@gmail.com>
2019-02-28 03:22:44 +0000

    add 100-wildcmp.c

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -50,7 +50,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (!*s1)
 			return (1);
-		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	if (!*s1 || !s2)
 		return (0);

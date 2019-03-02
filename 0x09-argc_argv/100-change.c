25f8ce2247d8bbf2251d97d82d44e11e5ca35454
Arthur Damm <indigoarthur@gmail.com>
2019-03-02 14:51:01 +0000

    add 100-change.c

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - it all starts here
+ * @argc: the number of arguments
+ * @argv: array of pointers to arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, n, c = 0, COINS[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+		return (printf("Error\n"), 1);
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] < '0' || argv[1][i] > '9')
+			return (printf("Error\n"), 1);
+	n = atoi(argv[1]);
+	if (n < 0)
+		return (puts("0"), 1);
+	for (i = 0; i < 5; i++)
+		if (n / COINS[i])
+		{
+			c += n / COINS[i];
+			n %= COINS[i];
+		}
+	printf("%d\n", c);
+	return (0);
+}

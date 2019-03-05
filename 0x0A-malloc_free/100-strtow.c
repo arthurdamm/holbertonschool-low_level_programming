bf28830c2e0d9fc0d72269639bcb724dd7046342
Arthur Damm <indigoarthur@gmail.com>
2019-03-05 23:31:06 +0000

    add 100-strtow.c

---
+++ 0x0A-malloc_free/100-strtow.c
@@ -0,0 +1,79 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * copychars - copies chars to buffer
+ * @b: destination buffer
+ * @start: starting char pointer
+ * @stop: ending char pointer
+ */
+void copychars(char *b, char *start, char *stop)
+{
+	while (start <= stop)
+		*b++ = *start++;
+	*b = '\0';
+}
+
+/**
+ * wordcount - counts the number of words
+ * @str: the sentence string
+ *
+ * Return: int number of words
+ */
+int wordcount(char *str)
+{
+	int words = 0, in_word = 0;
+
+	while (*str)
+	{
+		if (*str == ' ')
+		{
+			if (in_word)
+				words++;
+			in_word = 0;
+		}
+		else
+			in_word++;
+		str++;
+	}
+	return (words);
+}
+
+/**
+ * strtow - splits sentence into words
+ * @str: the sentence string
+ *
+ * Return: pointer to string array
+ */
+char **strtow(char *str)
+{
+	int words = 0, in_word = 0;
+	char **ret, *word_start;
+
+	if (!str || !*str)
+		return (NULL);
+	ret = malloc(sizeof(char *) * (wordcount(str) + 1));
+	while (*str)
+	{
+		if (*str == ' ')
+		{
+			if (in_word)
+			{
+				ret[words] = malloc(sizeof(char) * (in_word + 1));
+				if (!ret[words])
+					return (NULL);
+				copychars(ret[words], word_start, str - 1);
+				words++;
+				in_word = 0;
+			}
+		}
+		else
+		{
+			if (!in_word++)
+				word_start = str;
+		}
+		str++;
+	}
+	ret[words] = NULL;
+	return (ret);
+}

84dc879cd507f4aefe469ab9965039c728770f0d
Arthur Damm <indigoarthur@gmail.com>
2019-03-05 20:12:27 +0000

    add 1-strdup.c

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}

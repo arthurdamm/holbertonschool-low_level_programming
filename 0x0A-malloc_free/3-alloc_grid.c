29696c3aa4d4669b0a72527308ae12720087a2c6
Arthur Damm <indigoarthur@gmail.com>
2019-03-05 20:12:50 +0000

    add 3-alloc_grid.c

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - allocates a two dimensional array
+ * @width: the table width
+ * @height: the table height
+ *
+ * Return: pointer to the table
+ */
+int **alloc_grid(int width, int height)
+{
+	int h2 = height;
+	int **rows = malloc(sizeof(int *) * height);
+
+	if (!rows)
+		return (NULL);
+	while (height--)
+	{
+		int *cols = malloc(sizeof(int) * width);
+		int i = width;
+
+		if (!cols)
+			return (NULL);
+		while (i--)
+			*cols++ = 0;
+		*rows++ = cols - width;
+	}
+	return (rows - h2);
+}

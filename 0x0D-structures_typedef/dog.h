53eff3d7f21d2d2848c4a7ef32248a46caf78a24
Arthur Damm <indigoarthur@gmail.com>
2019-03-11 20:13:12 +0000

    add dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: just a lone dog struct in a big kitty world
+ */
 struct dog
 {
 	char *name;
@@ -9,3 +17,4 @@ struct dog
 };
 
 #endif
+	

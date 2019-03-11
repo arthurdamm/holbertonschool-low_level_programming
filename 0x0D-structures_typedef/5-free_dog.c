ac113d597a2c0e9271102073548408fac7c94af7
Arthur Damm <indigoarthur@gmail.com>
2019-03-11 20:48:37 +0000

    add 5-free_dog.c

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees yo dawgs
+ * @d: yo dawg
+ *
+ * Return: void.
+ */
+void free_dog(dog_t *d)
+{
+	if (!d)
+	{
+		if (d->name)
+			free (d->name);
+		if (d->owner)
+			free (d->owner);
+		free (d);
+	}
+}

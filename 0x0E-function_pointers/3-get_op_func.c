e59c954d7988aff255d4af45fc840fc4632222d6
Arthur Damm <indigoarthur@gmail.com>
2019-03-13 23:01:27 +0000

    number 3

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+
+/**
+* get_op_func - returns pointer to right op func
+* @s: string operator
+*
+* Return: the right function pointer
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	for (i = 0; i <= 5; i++)
+	{
+		if (s && s[0] == ops[i].op[0])
+			return (ops[i].f);
+	}
+
+	return (NULL);
+}
---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod -  modulo two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

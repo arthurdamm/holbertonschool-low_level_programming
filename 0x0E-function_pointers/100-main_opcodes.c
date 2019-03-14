ef21372ad93700958154ae5db4209af7784edc6a
Arthur Damm <indigoarthur@gmail.com>
2019-03-14 00:02:58 +0000

    add 100-main_opcodes.c

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: the number of args
+ * @argv: argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+
+	while (b--)
+		printf("%hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}

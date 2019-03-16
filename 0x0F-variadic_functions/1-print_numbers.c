318d1a129ca68eeccd148fec1c072ee415ef94b1
Arthur Damm <indigoarthur@gmail.com>
2019-03-16 19:20:21 +0000

    add 1-print_numbers.c

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -15,7 +15,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 
 	if (!n)
+	{
+		printf("\n");
 		return;
+	}
 	va_start(ap, n);
 	while (i--)
 		printf("%d%s", va_arg(ap, int),

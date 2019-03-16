785e9ad5f0f7e86f98422826f57867b7b61fd726
Arthur Damm <indigoarthur@gmail.com>
2019-03-16 19:04:42 +0000

    header

---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,19 @@
+#ifndef VARIADIC_H
+#define VARIADIC_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif

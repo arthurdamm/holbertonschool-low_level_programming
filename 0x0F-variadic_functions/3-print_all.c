116231d3aac0cecc373f2c4d91408bf1c744ea5c
Arthur Damm <indigoarthur@gmail.com>
2019-03-16 19:08:55 +0000

    add 3-print_all.c

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,20 +1,40 @@
 #include "variadic_functions.h"
 
+/**
+ * format_char - formats character
+ * @separator: the string seprator
+ * @ap: argument pointer
+ */
 void format_char(char *separator, va_list ap)
 {
 	printf("%s%c", separator, va_arg(ap, int));
 }
 
+/**
+ * format_int - formats integer
+ * @separator: the string seprator
+ * @ap: argument pointer
+ */
 void format_int(char *separator, va_list ap)
 {
 	printf("%s%d", separator, va_arg(ap, int));
 }
 
+/**
+ * format_float - formats float
+ * @separator: the string seprator
+ * @ap: argument pointer
+ */
 void format_float(char *separator, va_list ap)
 {
 	printf("%s%f", separator, va_arg(ap, double));
 }
 
+/**
+ * format_string - formats string
+ * @separator: the string seprator
+ * @ap: argument pointer
+ */
 void format_string(char *separator, va_list ap)
 {
 	char *str = va_arg(ap, char *);

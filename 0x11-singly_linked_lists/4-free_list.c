b17536404a20557a196fc1e4f1d162d9df3d5fd9
Arthur Damm <indigoarthur@gmail.com>
2019-03-21 21:15:32 +0000

    add 4-free_list.c

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - frees all nodes of a list
+ * @head: pointer to head node
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *node, *next_node;
+
+	if (!head)
+		return;
+
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node);
+		node = next_node;
+	}
+}

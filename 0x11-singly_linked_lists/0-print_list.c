3300e9baf6a012a0a06feb0c4b54a7dd0fb3fe2d
Arthur Damm <indigoarthur@gmail.com>
2019-03-21 16:56:03 +0000

    add 0-print_list.c

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_list - prints a linked lists
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", h->len, h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

326e29395dc8b0f4ea6e41a71a313e96f6f6bfab
Arthur Damm <indigoarthur@gmail.com>
2019-03-21 20:56:28 +0000

    add 1-list_len.c

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return (0);
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
  * list_len - determines length of linked list
  * @h: pointer to first node
  *

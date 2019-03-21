fa1b370d9141b0296d0baabdf334e83511283c4c
Arthur Damm <indigoarthur@gmail.com>
2019-03-21 21:04:12 +0000

    add 2-add_node.c

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -17,11 +17,14 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		new_head->str = strdup(str);
 		if (!new_head->str)
+		{
+			free(new_head);
 			return (NULL);
+		}
 		new_head->len = _strlen(new_head->str);
 	}
 	else
-		return (NULL);
+
 	new_head->next = *head;
 	*head = new_head;
 	return (new_head);

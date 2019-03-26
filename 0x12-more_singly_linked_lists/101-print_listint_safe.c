32d2f5b9a3d4b66a363bc89a1c4a5890c2cb23c7
Arthur Damm <indigoarthur@gmail.com>
2019-03-26 15:49:16 +0000

    try remove exit

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,12 +9,14 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t c = 0;
+	long diff;
 
 	while (head)
 	{
 		c++;
+		diff = head - head->next;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (head < head->next)
+		if (diff < 0)
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;

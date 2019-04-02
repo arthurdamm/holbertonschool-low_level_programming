05f66b160deaeecae372947d03f9af7c0c95d912
Arthur Damm <indigoarthur@gmail.com>
2019-04-02 23:58:39 +0000

    write to STDERR

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -38,26 +38,26 @@ int main(int ac, char **av)
 	char buf[READ_BUF_SIZE];
 
 	if (ac != 3)
-		dprintf(STDOUT_FILENO, USAGE), exit(97);
+		dprintf(STDERR_FILENO, USAGE), exit(97);
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
-		dprintf(STDOUT_FILENO, ERR_NOREAD, av[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_fd == -1)
-		close(from_fd), dprintf(STDOUT_FILENO, ERR_NOWRITE, av[2]), exit(99);
+		close(from_fd), dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
 	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
 		if (write(to_fd, buf, b) != b)
-			dprintf(STDOUT_FILENO, ERR_NOWRITE, av[2]), exit(99);
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	if (b == -1)
-		dprintf(STDOUT_FILENO, ERR_NOREAD, av[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
 	from_fd = close(from_fd);
 	to_fd = close(to_fd);
 	if (from_fd)
-		dprintf(STDOUT_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 	if (to_fd)
-		dprintf(STDOUT_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 
 	return (EXIT_SUCCESS);
 }

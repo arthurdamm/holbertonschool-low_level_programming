36753682d9aa181b48efaae4f461d13a04421444
Arthur Damm <indigoarthur@gmail.com>
2019-04-02 21:32:33 +0000

    fix error return code

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-		return (0);
+		return (-1);
 	if (len)
 		bytes = write(fd, text_content, len);
 	close(fd);

a8352287a64091c68d987e8ed5f9fd001edd5efb
Arthur Damm <indigoarthur@gmail.com>
2019-04-02 22:12:34 +0000

    fix buffer

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -11,15 +11,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes;
-	char buf[READ_BUF_SIZE];
+	char buf[READ_BUF_SIZE*8];
 
 	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	bytes = read(fd, buf, letters);
-	bytes = write(STDOUT_FILENO, buf, bytes);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
 	return (bytes);
 }

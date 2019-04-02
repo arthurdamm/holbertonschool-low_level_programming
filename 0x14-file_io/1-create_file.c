8ad21ea890dd8921993e557cac8631d067503dd6
Arthur Damm <indigoarthur@gmail.com>
2019-04-02 21:31:48 +0000

    check len

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -25,14 +25,15 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes, len = _strlen(text_content);
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (0);
-	bytes = write(fd, text_content, len);
+	if (len)
+		bytes = write(fd, text_content, len);
 	close(fd);
 	return (bytes == len ? 1 : -1);
 }

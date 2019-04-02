68f4014fdedaf817521693b8637aafc31e318fac
Arthur Damm <indigoarthur@gmail.com>
2019-04-02 21:26:58 +0000

    1-create_file.c

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: text to write
+ *
+ * Return: 1 on success 0 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (0);
+	bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}

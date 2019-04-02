895d4ef03cd49035b5ca5a5cd67f302c49f91f3a
Arthur Damm <indigoarthur@gmail.com>
2019-04-02 23:12:34 +0000

    add 2-append

---
+++ 0x14-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: name of file to create
+ * @text_content: text to write
+ *
+ * Return: 1 on success 0 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}

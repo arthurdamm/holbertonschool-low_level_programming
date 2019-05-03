ce692e1c7ee4f61c6028534a6850220d2efbe991
Arthur Damm <indigoarthur@gmail.com>
2019-05-03 21:01:22 +0000

    add 0-print_dlistint.c

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints doubly-linked list
+ * @h: address of head node
+ *
+ * Return: size of list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	printf("-> %d elements\n", i);
+	return (i);
+}

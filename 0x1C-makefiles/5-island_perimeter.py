4c17414250ca8e5db4e0e3815b6f8c92f684ffb1
Arthur Damm <indigoarthur@gmail.com>
2019-07-11 21:49:11 +0000

    add 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,9 +16,13 @@ def island_perimeter(grid):
     ret = 0
     for y, row in enumerate(grid):
         for x, cell in enumerate(row):
-            if cell:
-                ret += y == 0 or not grid[y - 1][x]
-                ret += y == len(grid) - 1 or not grid[y + 1][x]
-                ret += x == 0 or not grid[y][x - 1]
-                ret += x == len(grid[0]) - 1 or not grid[y][x - 1]
+            if cell == 1:
+                if y == 0 or grid[y - 1][x] == 0:
+                    ret += 1
+                if y == len(grid) - 1 or grid[y + 1][x] == 0:
+                    ret += 1
+                if x == 0 or grid[y][x - 1] == 0:
+                    ret += 1
+                if x == len(row) - 1 or grid[y][x - 1] == 0:
+                    ret += 1
     return ret

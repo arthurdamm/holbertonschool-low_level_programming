deccf9961f70e9ac9bdb2e6fa0c5479f8c97f096
Arthur Damm <indigoarthur@gmail.com>
2019-07-11 21:39:54 +0000

    add 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,27 +4,15 @@ Module for island_perimeter() method
 """
 
 
-def count_water(grid, row, col):
-    """
-    Counts how many water tiles surround this tile.
-    """
-    ret = 0
-    ret += row == 0 or not grid[row - 1][col]
-    ret += row == len(grid) - 1 or not grid[row + 1][col]
-    ret += col == 0 or not grid[row][col - 1]
-    ret += col == len(grid[0]) - 1 or not grid[row][col - 1]
-    return ret
-
-
 def island_perimeter(grid):
     """
     Computers the length of the perimeter of an island.
     """
     if not grid or type(grid) != list or not len(grid):
-        return 0
+        return None
     for row in grid:
         if not row or type(row) != list or len(row) != len(grid[0]):
-            return 0
+            return None
     ret = 0
     for y, row in enumerate(grid):
         for x, cell in enumerate(row):

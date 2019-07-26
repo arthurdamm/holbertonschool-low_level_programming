795b4ee8d485b6b30d20caf262840ff1abca81ea
Arthur Damm <indigoarthur@gmail.com>
2019-07-26 21:15:52 +0000

    add 0x1D-binary_trees/

--- 0x1D-binary_trees/120-binary_tree_is_avl.c
+++ 0x1D-binary_trees/120-binary_tree_is_avl.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+ * _binary_tree_height - helper func for binary_tree_height
+ * @tree: input tree
+ * Return: height of tree
+ */
+size_t _binary_tree_height(const binary_tree_t *tree)
+{
+	size_t a, b;
+
+	if (!tree)
+		return (0);
+
+	a = _binary_tree_height(tree->left);
+	b = _binary_tree_height(tree->right);
+	return (MAX(a, b) + 1);
+}
+
+/**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: input tree
  * @min: minimum value of n

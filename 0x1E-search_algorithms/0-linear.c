303f3b609a9017e0a1f0e65f010076ba70f9500c
Arthur Damm <indigoarthur@gmail.com>
2019-08-27 20:26:05 +0000

    Check if array null

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,6 +12,8 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
+	if (!array)
+		return (-1);
 	while (i < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);

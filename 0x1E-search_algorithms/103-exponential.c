8dbd80b2b35b53382b49405e382cd6fa1b342789
Arthur Damm <indigoarthur@gmail.com>
2019-08-28 20:54:41 +0000

    add 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -11,6 +11,7 @@
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t i = 1, newsize = 0;
+	int ret;
 
 	if (!array || !size)
 		return (-1);
@@ -20,11 +21,12 @@ int exponential_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		i <<= 1;
 	}
-	newsize = (i > size ? size : i + 1) - (i >> 1);
+	newsize = (i >= size ? size - 1 : i + 1) - (i >> 1);
 	i >>= 1;
 	printf("Value found between indexes [%lu] and [%lu]\n",
 			i, i << 1 >= size ? size - 1 : i << 1);
-	return (binary_search(array + i, newsize, value));
+	ret = binary_search(array + i, newsize, value);
+	return (ret == -1 ? ret : ret + (int)i);
 }
 
 /**
@@ -38,6 +40,7 @@ int exponential_search(int *array, size_t size, int value)
 int binary_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
+	int *a = array;
 
 	if (!array)
 		return (-1);
@@ -45,16 +48,16 @@ int binary_search(int *array, size_t size, int value)
 	while (size)
 	{
 		for (i = 0, printf("Searching in array: "); i < size; i++)
-			printf("%d%s", array[i], i + 1 == size ? "\n" : ", ");
+			printf("%d%s", a[i], i + 1 == size ? "\n" : ", ");
 
 		i = (size - 1) / 2;
-		if (array[i] == value)
-			return (value);
-		else if (array[i] > value)
+		if (a[i] == value)
+			return ((a - array) + i);
+		else if (a[i] > value)
 			size = i;
 		else
 		{
-			array += (i + 1);
+			a += (i + 1);
 			size -= (i + 1);
 		}
 	}

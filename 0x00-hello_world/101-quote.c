f5c7607d85f1ba788f8ef6722e8f1b67b9a63311
Arthur Damm <indigoarthur@gmail.com>
2020-01-27 16:30:52 -0800

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	write(2, "aand that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
